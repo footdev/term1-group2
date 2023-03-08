0a1b0617fd991245360bb214d00fb01498ba879f
ki_mensoo <ekclstkfka5@gmail.com>
2023-03-09 01:05:59 +0900

    feat: [BOJ17298] 오큰수 풀이 완료
    BOJ17298 오큰수 풀이를 완료함.

--- hyeonsu/baekjoon/BOJ17298.java
+++ hyeonsu/baekjoon/BOJ17298.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class BOJ17298 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
     static int n;
@@ -16,32 +17,46 @@ public class BOJ17298 {
         n = Integer.parseInt(br.readLine());
         in = new int[n];
         ohKen = new int[n];
-        ohKen[n - 1] = -1;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             in[i] = Integer.parseInt(st.nextToken());
         }
 
-        s.push(in[n - 1]);
+        s.push(0);
 
-        for (int i = n - 2; i >= 0; i--) {
-            //현재 값이 오른쪽에 있는 가장 큰 값보다 작은 경우
-            if (s.peek() >= in[i]) {
-                ohKen[i] = s.peek();
-                s.push(ohKen[i]);
+        for (int i = 1; i < n; i++) {
+            if (s.isEmpty()) {
+                ohKen[i] = -1;
+                s.push(i);
+                continue;
             }
-            //현재 값이 오른쪽 값보다 클 경우
-            else if (s.peek() < in[i]) {
-                //그 다음 값들의 오큰 수를 차례로 본다.
-                for (int j = i; j < n; j++) {
-                    //
+            if (in[s.peek()] >= in[i]) {
+                s.push(i);
+            } else {
+                while (!s.isEmpty()) {
+                    if (in[s.peek()] < in[i]) {
+                        ohKen[s.pop()] = in[i];
+                    } else {
+                        s.push(i);
+                        break;
+                    }
                 }
+                s.push(i);
+            }
+        }
+
+        if (!s.isEmpty()) {
+            while (!s.isEmpty()) {
+                ohKen[s.pop()] = -1;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.print(ohKen[i] + " ");
+            bw.write(ohKen[i] + " ");
         }
+        bw.write("\n");
+        bw.flush();
+        bw.close();
     }
 }
