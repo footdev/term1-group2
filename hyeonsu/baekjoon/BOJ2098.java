43b0878231c7cb2b0ba1ee6d8a7611ba598f51a8
ekclstkfka5 <ekclstkfka5@gmail.com>
2023-04-07 09:42:39 +0900

    feat: [BOJ2098] 외판원 순회 풀이 완료
    BOJ2098 외판원 순회 풀이를 완료함.

--- hyeonsu/baekjoon/BOJ2098.java
+++ hyeonsu/baekjoon/BOJ2098.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class BOJ2098 {
 	
+	static final int INF = 1000000000;
+	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	
@@ -26,7 +28,7 @@ public class BOJ2098 {
 		
 		int ans = tsp(0, 0);
 		
-		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+		System.out.println(ans == INF ? -1 : ans);
 	}
 	
 	static int tsp(int visited, int now) {
@@ -36,17 +38,26 @@ public class BOJ2098 {
 		// 모든 도시를 지난 경우
 		if (visited == (1 << N) - 1) {
 			//now -> 0 (출발 도시)로 가능 경로가 있어야 돌아갈 수 있음
-			//여행비용 반환
+			if (w[now][0] > 0) return w[now][0];
 			//없는 경우 MAX 반환
+			return INF;
+		}
+
+		// 지금 방문중인 도시와 지금까지 방문한 도시들이 같고 이미 다녀온 중복된 경로라면 Memoization 활용
+		if (dp[visited][now] > 0) return dp[visited][now];
+		
+		//방문하지 않은 도시들 중 가는 경로가 있는 도시를 전부 다 가본다.
+		int min = INF;
+		for (int i = 0; i < N; i++) {
+			if (i != now && (visited & (1 << i)) == 0 && w[now][i] > 0) {
+				//최소비용 갱신
+				int tmp = tsp(visited, i) + w[now][i];
+				//max값 갱신
+				min = Math.min(min, tmp);
+			}
 		}
 		
-		// 지금 방문중인 도시와 지금까지 방문한 도시들이 같고 이미 다녀온 중복된 경로라면 Memoization 활용
-		
-		//방문하지 않은 도시 들 중 가는 경로가 있는 도시를 전부 다 가본다.
-		int max = Integer.MAX_VALUE;
-			//최소비용 갱신
-			//max값 갱신
-		return dp[visited][now] = max;
+		return dp[visited][now] = min;
 	}
 
 	static int stoi(String s) {return Integer.parseInt(s);}
